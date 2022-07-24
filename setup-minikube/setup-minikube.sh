8332e1e1efdb2ce43e548c90ecb16d3fb8dc48eb
Abdelmajid Ouelbani <ouelbani.abdelmajid@gmail.com>
2022-07-24 22:53:00 +0200

    Install docker last

--- setup-minikube/setup-minikube.sh
+++ setup-minikube/setup-minikube.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Execute files in order
-for filename in *.sh; do
-        sh $filename
-done
-
 # Alias preferences
 echo 'alias l="ls -la"' >>~/.bashrc
 echo 'alias kc="kubectl"' >>~/.bashrc
 echo 'alias kca="kubectl apply -f"' >>~/.bashrc
 
 # Setup autocompletions
-echo "$(minikube completion bash)" >>~/.bashrc
-echo "eval \"$(kubectl completion bash)\"" >>~/.bashrc
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+echo 'source <(minikube completion bash)' >>~/.bashrc
 
 source ~/.profile
+
+# Execute files in order
+for filename in *.sh; do
+        sh $filename
+done
