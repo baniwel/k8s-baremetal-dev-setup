cd290a22c9c861255f15ebb43845e52733e780ff
Abdelmajid Ouelbani <ouelbani.abdelmajid@gmail.com>
2022-07-30 00:11:24 +0200

    missing dependency

--- setup-minikube/02-kubectl-install.sh
+++ setup-minikube/02-kubectl-install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Install dependencies"
-sudo apt-get update && sudo apt-get install -y apt-transport-https > /dev/null
+sudo apt-get update
+sudo apt-get install -y apt-transport-https > /dev/null
+sudo apt-get install -y gnupg2 > /dev/null
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
