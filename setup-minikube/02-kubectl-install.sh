27bd0fb3d415ce06d471982dbf2374e3c3508964
Abdelmajid Ouelbani <ouelbani.abdelmajid@gmail.com>
2022-07-29 23:42:10 +0200

    Fix installs

--- setup-minikube/02-kubectl-install.sh
+++ setup-minikube/02-kubectl-install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# TODO check why this script needs to be ran twice for kubectl to be usable.
-sudo apt-get update && sudo apt-get install -y apt-transport-https
+echo "Install dependencies"
+sudo apt-get update && sudo apt-get install -y apt-transport-https > /dev/null
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+
+echo "Install kubectl"
 sudo apt-get update
-sudo apt-get install -y kubectl
+sudo apt-get install -y kubectl > /dev/null
+
+echo "Link kube"
 sudo ln -s /usr/bin/kubectl /usr/local/bin/kubectl
